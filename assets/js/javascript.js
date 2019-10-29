d278208295e0d8af7e2c3d21410bb3cc12f99e2c
Marion <jolym8@gmail.com>
2019-10-29 11:33:17 +0100

    debut form

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,1 +1,24 @@
+var formValid = document.getElementById('bouton_envoi')
 
+var nom = document.getElementById('nom')
+var missNom = document.getElementById('missNom')
+
+var prenom = document.getElementById('prenom')
+var missPrenom = document.getElementById('missPrenom')
+
+var mail = document.getElementById('mail')
+var missMail = document.getElementById('missMail')
+
+var message = document.getElementById('message')
+var missMessage = document.getElementById('missMessage')
+
+formValid.addEventListener('click', validation);
+
+function validation(event){
+
+  if (prenom.validity.valueMissing) {
+    event.preventDefault();
+    missPrenom.textContent ='Prénom manquant';
+    missPrenom.style.color ='red';
+  }
+}
--- contact.php
+++ contact.php
@@ -1,4 +1,3 @@
-
 <?php  
 $ma_pages = 'contact';
 require("header.php");
@@ -6,52 +5,63 @@ require("header.php");
 
 ?>
 <!-- ------------------------------------------------------------------------------------------------------------------------ -->
-          <!--  -->
-          <!-- ------------------------------------------------------------------------------------------------------------------------ -->
-          
-          <div class="container">
-            <div class="row">
-              <div class="col-md-12">
-                  
-              <form action="traitement.php" method="post">
+<!--  -->
+<!-- ------------------------------------------------------------------------------------------------------------------------ -->
 
-<label for="nom">Nom</label>
-<input type="text" name="nom" id="nom"><br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
 
-<label for="prenom">Prenom</label>
-<input type="text" name="prenom" id="prenom"><br>
+      <form action="traitement.php" method="post">
 
-<label for="mail">Adresse e-mail</label>
-<input type="mail" name="mail" id="mail"><br>
-
-<label for="message">Votre Message</label>
-<textarea name="message" id="message" ></textarea> <br>
-
-<input type="submit" value="Envoyer">
-<input type="reset" value="Annuler">
+        <label for="nom">Nom</label>
+        <input type="text" name="nom" id="nom" required><br>
+        <span id="missNom"></span><br>
 
 
-              </form>
-                
-              </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div>
-                        <img class="d-block w-100" src="assets/img/ban_yt.JPG" alt="banniere">
-  
-                    </div>
-  
-                </div>
-          </div>
-          <!-- ------------------------------------------------------------------------------------------------------------------------ -->
-          <!-- Footer -->
-        <!-- ------------------------------------------------------------------------------------------------------------------------ -->
-        <!-- Logo -->
-        <!-- Contact -->
-        <!-- copiright Mentions légales -->
-        <!-- Cookies -->
+        <label for="prenom">Prenom</label>
+        <input type="text" name="prenom" id="prenom"><br>
+        <span id="missPrenom"></span><br>
+
+
+        <label for="mail">Adresse e-mail</label>
+        <input type="email" name="mail" id="mail" required><br>
+        <span id="missMail"></span><br>
+
+
+        <label for="">Votre Message</label>
+        <textarea name="message" id="message" required></textarea> <br>
+        <span id="missMessage"></span><br>
+
+        <input type="submit" value="Envoyer" id="bouton_envoi">
+        <input type="reset" value="Annuler">
+
+
+      </form>
+
+
+
     </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12">
+      <div>
+        <img class="d-block w-100" src="assets/img/ban_yt.JPG" alt="banniere">
+
+      </div>
+
+    </div>
+  </div>
+  <!-- ------------------------------------------------------------------------------------------------------------------------ -->
+  <!-- Footer -->
+  <!-- ------------------------------------------------------------------------------------------------------------------------ -->
+  <!-- Logo -->
+  <!-- Contact -->
+  <!-- copiright Mentions légales -->
+  <!-- Cookies -->
+</div>
+
+<script src="assets\js\javascript.js"></script>
 
 <?php  
 require("footer.php")
