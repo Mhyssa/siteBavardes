6df0ebc78af4023af866db1ed961b8388d438f93
noemie <noemie.cld@outlook.fr>
2019-11-02 20:20:28 +0100

    form

--- traitement.php
+++ traitement.php
@@ -1,8 +1,19 @@
 <?php
-$formError = array();
+$to = "staffbavardes@gmail.com";
+// $formError = array();
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$mail = $_POST['mail'];
+$question = $_POST['question'];
+$headers  =     'from '.$mail;
+$headers .= 'reply-to: '.$to;
+// $headers .=   'reply '.$to;
+$headers.=   'x-mailer '.'PHP/'.phpversion();
+
+
 
 $mailPattern = '/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/';
-
+ mail ($to,$question,$headers);
 // if (isset($_POST['submit'])) {
    
 //     if (!empty($_POST['mail'])) {
@@ -15,5 +26,5 @@ $mailPattern = '/[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}/';
 //         $formError['mail'] = 'Veuillez renseigner le champs "Email"';
 //     }
 
-    header("Location:contact_merci.php");
+   
 ?>
