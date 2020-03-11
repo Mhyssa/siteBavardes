1d5f763e7711e778bafb4067de32d90e185701fd
Mhyssa <jolym8@gmail.com>
2020-03-11 21:36:13 +0100

    modif en page

--- collaboration.php
+++ collaboration.php
@@ -5,9 +5,11 @@ require("header.php");
 ?>
 <div class="col-lg-12 ombre pt-5 ">
   <div class="container">
-  <div class="row">
-                  <img src="assets\img\banniere\collab.png" alt="banniere collaboration DRJSCS DDCS Amiens Métropole Amiens Métropole Natation La LIG" class="img-fluid pb-3">
-            </div>
+    <div class="row">
+      <img src="assets\img\banniere\collab.png"
+        alt="banniere collaboration DRJSCS DDCS Amiens Métropole Amiens Métropole Natation La LIG"
+        class="img-fluid pb-3">
+    </div>
 
     <div class="row">
 
@@ -27,7 +29,6 @@ require("header.php");
           </div>
         </div>
       </div>
-
       <div class="card mb-3" style="max-width: 540px; ">
         <div class="row no-gutters">
           <div class="col-md-4">
@@ -45,170 +46,166 @@ require("header.php");
           </div>
         </div>
       </div>
-
-
-
-   
-
-    <div class="row pt-5">
-      <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="assets\img\collaborateurs\pride.jpg" class="card-img d-block w-100" alt="Collectif Idahot">
+      <div class="row pt-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="assets\img\collaborateurs\pride.jpg" class="card-img d-block w-100" alt="Collectif Idahot">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Idahot</h5>
+                <a class="card-text link"
+                  href="https://www.facebook.com/events/gare-damiens/1%C3%A8re-marche-des-fiert%C3%A9s-2019-amiens/396220987643121/">Premiere
+                  marche des fiertés Amiens</a>
+                <p class="card-text"><small class="text-muted">Festival Idahot</small></p>
+              </div>
+            </div>
           </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Idahot</h5>
-              <a class="card-text link"
-                href="https://www.facebook.com/events/gare-damiens/1%C3%A8re-marche-des-fiert%C3%A9s-2019-amiens/396220987643121/">Premiere
-                marche des fiertés Amiens</a>
-              <p class="card-text"><small class="text-muted">Festival Idahot</small></p>
+        </div>
+
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="assets\img\collaborateurs\ob.jpg" class="card-img d-block w-100" alt="Radios campus amiens">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Radio Campus Amiens</h5>
+                <a class="card-text link" href="http://www.radiocampusamiens.fr/emissions/ovairesbookees/">Radio
+                  Campus Amiens</a>
+                <p class="card-text"><small class="text-muted">Ovaires Boockées</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row pt-5">
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="assets\img\collaborateurs\michelis.jpg" class="card-img d-block w-100"
+                alt="Interventions en milieu scolaire lycée michelis">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Lycée Michelis</h5>
+                <a class="card-text link" href="action.php">Interventions en milieu scolaire</a>
+                <p class="card-text"><small class="text-muted"></small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card mb-3" style="max-width: 540px;">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img src="assets\img\collaborateurs\alimentation.jpg" class="card-img d-block w-100"
+                alt="alimentation général pinkmoloko">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">L'alimentation Générale</h5>
+
+                <a class="card-text link" href="http://alimentation-generale.net/evenements/2019-10-04#.Xb2_6ppKiUk">NDA
+                  SPÉCIALE MEUFS AUX PLATINES W/ MORELLO, IZZY, STELLA & MORE</a>
+                <p class="card-text"><small class="text-muted"></small></p>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="assets\img\collaborateurs\ob.jpg" class="card-img d-block w-100" alt="Radios campus amiens">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Radio Campus Amiens</h5>
-              <a class="card-text link" href="http://www.radiocampusamiens.fr/emissions/ovairesbookees/">Radio
-                Campus Amiens</a>
-              <p class="card-text"><small class="text-muted">Ovaires Boockées</small></p>
-            </div>
-          </div>
-        </div>
-      </div>
+
+
     </div>
-    <div class="row pt-5">
-      <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="assets\img\collaborateurs\michelis.jpg" class="card-img d-block w-100"
-              alt="Interventions en milieu scolaire lycée michelis">
+
+
+
+
+    <!-- -le mois des violences faites aux femmes 2017 page facebook Amiens non aux violences faite aux femmes  -->
+
+
+
+
+
+
+    <div class="row">
+      <div class="col-md-12">
+        <br><br>
+        <h4 id="titre">
+          Merci de votre confiance et de votre chaleureux acceuil :
+
+        </h4>
+        <br>
+        <div class="row">
+          <div class="col-md-3 col-12">
+
+            <ul>
+              <li>Le Network</li>
+              <li>Le Barrio</li>
+              <li>La Gargouille</li>
+              <li>Le Charleston</li>
+              <li>Le Café</li>
+              <li>Le Capuccino</li>
+              <li>La boîte sans projet</li>
+              <li>Centre Leo Lagrange</li>
+              <li>Le Mic Mac</li>
+              <li>Les Enfants Terribles</li>
+
+
+            </ul>
+
           </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Lycée Michelis</h5>
-              <a class="card-text link" href="action.php">Interventions en milieu scolaire</a>
-              <p class="card-text"><small class="text-muted"></small></p>
-            </div>
+          <div class="col-md-3 col-12">
+            <ul>
+              <li>Le Red & White</li>
+              <li>La peniche du lapin vert</li>
+              <li>L'île aux fruits</li>
+              <li>Le füt et à mesure</li>
+              <li>La Taverne électrique</li>
+              <li>Sports bar </li>
+              <li> CSC d'étouvie</li>
+              <li> La lune des pirates</li>
+              <li> Lycée Michelis</li>
+              <li> Lycée La Hotoie</li>
+
+
+            </ul>
+          </div>
+          <div class="col-md-3 col-12">
+            <ul>
+              <li> Groupe d'Appui et de solidarité</li>
+              <li> Radio Campus</li>
+              <li> Carmen</li>
+              <li> Translucide</li>
+              <li> Flash our true colors </li>
+              <li> Jeanne Magazine</li>
+              <li> OSAM</li>
+              <li> UPJV Amiens</li>
+              <li> Cité Carter</li>
+              <li> AIDES</li>
+
+            </ul>
+          </div>
+
+          <div class="col-md-3 col-12">
+            <ul>
+              <li>Collectif IDAHOT</li>
+              <li> L'alimentation générale</li>
+              <li> DRJSCS</li>
+              <li> DDCS</li>
+              <li> Amiens Métropole</li>
+              <li> Amiens Métropole Natation</li>
+              <li> La LIG</li>
+              <li> La cave du houblon</li>
+            </ul>
           </div>
         </div>
       </div>
 
-      <div class="card mb-3" style="max-width: 540px;">
-        <div class="row no-gutters">
-          <div class="col-md-4">
-            <img src="assets\img\collaborateurs\alimentation.jpg" class="card-img d-block w-100"
-              alt="alimentation général pinkmoloko">
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">L'alimentation Générale</h5>
-
-              <a class="card-text link" href="http://alimentation-generale.net/evenements/2019-10-04#.Xb2_6ppKiUk">NDA
-                SPÉCIALE MEUFS AUX PLATINES W/ MORELLO, IZZY, STELLA & MORE</a>
-              <p class="card-text"><small class="text-muted"></small></p>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-
-
-
   </div>
-
-
-
-
-  <!-- -le mois des violences faites aux femmes 2017 page facebook Amiens non aux violences faite aux femmes  -->
-
-
-
-
-
-
-  <div class="row">
-    <div class="col-md-12">
-      <br><br>
-      <h4 id="titre">
-        Merci de votre confiance et de votre chaleureux acceuil :
-
-      </h4>
-      <br>
-      <div class="row">
-      <div class="col-md-3 col-12">
-      
-      <ul >
-        <li>Le Network</li>
-        <li>Le Barrio</li>
-        <li>La Gargouille</li>
-        <li>Le Charleston</li>
-        <li>Le Café</li>
-        <li>Le Capuccino</li>
-        <li>La boîte sans projet</li>
-        <li>Centre Leo Lagrange</li>
-        <li>Le Mic Mac</li>
-        <li>Les Enfants Terribles</li>
-       
-       
-      </ul>
-      
-</div>
-<div class="col-md-3 col-12">
-<ul > 
-<li>Le Red & White</li>
-        <li>La peniche du lapin vert</li>
-        <li>L'île aux fruits</li> 
-        <li>Le füt et à mesure</li>
-        <li>La Taverne électrique</li>
-        <li>Sports bar </li>
-       <li> CSC d'étouvie</li>
-       <li> La lune des pirates</li>
-       <li> La cave du houblon</li>
-       <li> Lycée Michelis</li>
-       <li> Lycée La Hotoie</li>
-        
-        
-      </ul>
-      </div>
-      <div class="col-md-3 col-12">
-      <ul>
-     <li> Groupe d'Appui et de solidarité</li>
-       <li> Radio Campus</li>
-      <li>  Carmen</li>
-     <li> Translucide</li>
-     <li>   Flash our true colors </li>
-     <li>   Jeanne Magazine</li>
-     <li>   OSAM</li>
-      <li>  UPJV Amiens</li>
-      <li>  Cité Carter</li>
-      <li>  AIDES</li>
-        
-      </ul>
-      </div>
-      
-      <div class="col-md-3 col-12">
-      <ul ><li>Collectif IDAHOT</li>
-       <li> L'alimentation générale</li>
-      <li>  DRJSCS</li>
-       <li> DDCS</li>
-      <li>  Amiens Métropole</li>
-      <li>  Amiens Métropole Natation</li>
-      <li>  La LIG</li>
-        </ul>
-    </div>
-   </div>
-  </div>
-
-  </div>
-</div>
 </div>
 
 <?php  
