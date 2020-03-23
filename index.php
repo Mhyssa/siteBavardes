05fdcb7b329341e4531ca9017554bbb2e1449a17
Mhyssa <jolym8@gmail.com>
2020-03-23 14:04:42 +0100

    debut parallax page index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require("header.php");
 
 
   <div class="row m-0 mt-5">
-  <div class="col-md-12">
+  <div class="col-md-12 p-0">
     <a href="https://www.instagram.com/les.bavardes/" class="yellow float-left col-lg-3 col-sm-12">
       <div class="text-center">
         <img src="assets/img/navbar/instagram.png" class="imgSocialmedia">
@@ -32,9 +32,49 @@ require("header.php");
 
 
 <div class="container-fluid">
-  <div class="row m-0">
+
+    <!-- First Parallax Section -->
+<div class="jumbotron paral paralsec">
+    <h1 class="display-3">Here is a heading 1</h1>
+    <p class="lead">Here is a short description 1</p>
+    <p class="lead">
+    <a class="btn btn-info btn-lg btn-md" href="#" role="button">Here is a button 1</a>
+    </p>
+    </div>
+    
+    <!-- Second Parallax Section -->
+    <div class="jumbotron paral paralsec1">
+    <h1 class="display-3">Here is a heading 2</h1>
+    <p class="lead">Here is a short description 2</p>
+    <p class="lead">
+    <a class="btn btn-warning btn-lg btn-md" href="widgets.html" role="button">Here is a button 2</a>
+    </p>
+    </div>
+    
+    <!-- Third Parallax Section -->
+    <div class="jumbotron paral paralsec2">
+    <h1 class="display-3">Here is a heading 3</h1>
+    <p class="lead">Here is a short description 2</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg btn-md" href="themes.html" role="button">Here is a button 3</a>
+    </p>
+    </div>
+
+      <!-- Fourth Parallax Section -->
+      <div class="jumbotron paral paralsec2">
+    <h1 class="display-3">Here is a heading 3</h1>
+    <p class="lead">Here is a short description 2</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg btn-md" href="themes.html" role="button">Here is a button 3</a>
+    </p>
+    </div>  
+    <!-- Add More Parallax Sections Here -->
   </div>
-</div>
+
+
+  
+  </div>
+
 
 
 <div class="elfsight-app-64be235b-928e-4da1-8c53-ad8aeb5c55dc"></div>
