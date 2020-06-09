c6764bf464e9e21854749e43d65967fa675dd55b
wwejgu98 <58255425+wwejgu98@users.noreply.github.com>
2020-06-09 15:49:40 +0200

    Update account.php

--- application/views/users/account.php
+++ application/views/users/account.php
@@ -2,16 +2,16 @@
     <a class="mb-4" href="<?php echo base_url('users/logout'); ?>" class="float-right"><?php echo $account_logout; ?></a>
     <h3 class=" m-4">
         <p class="text-center">
-            <?php echo $account_wlc; ?> <?php echo $user['us_fname']; ?> sur l'espace membre du site des bavardes.
+            <?php echo $account_wlc; ?> <?php echo $user['us_fname']; ?> <?php echo $account_wlc_p2; ?>.
         </p>
         <p class="text-center">
-            Tiens toi informé sur nos prochains événements <a href="<?php echo base_url().'event/index'; ?>" class="btn btn-dark">ici <i class="fa fa-calendar" aria-hidden="true"></i></a>
+            <?php echo $account_btn_event; ?> <a href="<?php echo base_url().'event/index'; ?>" class="btn btn-dark"><?php echo $account_btn_event_ici; ?> <i class="fa fa-calendar" aria-hidden="true"></i></a>
         </p>
         <p class="text-center">
-            Si tu souhaites nous soutenir en adhérant à l'asso <a href="https://www.helloasso.com/associations/les-bavardes/adhesions/adhesion-2020" class="link" target="_blank"><button class="button2 btn-block">C'est par ici</button></a>
+            <?php echo $account_asso; ?> <a href="https://www.helloasso.com/associations/les-bavardes/adhesions/adhesion-2020" class="link" target="_blank"><button class="button2 btn-block mt-3"><?php echo $account_asso_ici; ?></button></a>
         </p>
     </h3>
-    <h4>Tes Informations personnelles : </h4>
+    <h4><?php echo $account_info ?> : </h4>
     <div class="mb-5">
         <p class="mb-4"><b><?php echo $account_name; ?>: </b><?php echo $user['us_fname'].' '.$user['us_lname']; ?></p>
         <p class="mb-4"><b><?php echo $account_mail; ?>: </b><?php echo $user['us_email']; ?></p>
